543eb6e0dbe40d75d56b6aff261ea9deccb3c858
Dnyaneshwari Bhakare <dnyaneshwaribhakare@gmail.com>
2022-10-14 18:44:33 -0700

    harane

---
+++ src/Data/reversestring.java
@@ -0,0 +1,14 @@
+package Data;
+
+public class reversestring {
+
+	public static void main(String[] args) {
+String s="Dnyanu";
+int b=s.length();
+for(int i=b-1;i>=0;i--) {
+	System.out.print(s.charAt(i));
+	
+}
+	}
+
+}
