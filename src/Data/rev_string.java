895bca4595873bb1c7d521c828fe432b69cc1292
bhushanharne <hbhushan38@gmail.com>
2022-10-15 07:14:35 +0530

    string class add

---
+++ src/Data/rev_string.java
@@ -0,0 +1,14 @@
+package Data;
+
+public class rev_string {
+
+	public static void main(String[] args) {
+
+String a ="bhushan";
+int b=a.length();
+for(int i=b-1;i>=0;i--) {
+	System.out.print(a.charAt(i));
+}
+	}
+
+}
